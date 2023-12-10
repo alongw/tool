d69030ef18a1fe4a31fe3cc2948409632242d0a0
alongw <alongw@alongw.cn>
2023-12-10 22:32:56 +0800

    chore: add dist dir

---
+++ dist/ncc/app.d.ts
@@ -0,0 +1,7 @@
+declare const _default: {
+    config: {
+        setConfig: <T>(path: string, encoding?: BufferEncoding) => boolean;
+        getConfig: <T_1>() => T_1;
+    };
+};
+export default _default;
---
+++ dist/ncc/modules/config.d.ts
@@ -0,0 +1,5 @@
+declare const _default: {
+    setConfig: <T>(path: string, encoding?: BufferEncoding) => boolean;
+    getConfig: <T_1>() => T_1;
+};
+export default _default;
