4b74eb018b527b98284e6eda9e12356c52ac39d8
alongw <alongw@alongw.cn>
2023-12-10 22:07:50 +0800

    feat(config): add config modules

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import config from './modules/config'
+
+export default {
+    config
+}
---
+++ src/modules/config.ts
@@ -0,0 +1,30 @@
+// 获取配置文件
+import fs from 'fs-extra'
+
+type Config<T> = T
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let configData: Config<any> | null = null
+
+const setConfig = <T>(path: string, encoding: BufferEncoding = 'utf-8'): boolean => {
+    try {
+        fs.readFileSync(path, {
+            encoding: encoding
+        })
+    } catch (error) {
+        return false
+    }
+    configData = fs.readFileSync(path, {
+        encoding: encoding
+    }) as T
+    return true
+}
+
+const getConfig = <T>(): Config<T> | undefined => {
+    return configData
+}
+
+export default {
+    setConfig,
+    getConfig
+}
