631edb746d2ab439d6c9759d0e0819d0de19e4b2
alongw <alongw@alongw.cn>
2023-12-10 22:42:01 +0800

    chore: remove dist dir

--- dist/out/app.d.ts
+++
@@ -1,7 +0,0 @@
-declare const _default: {
-    config: {
-        setConfig: <T>(path: string, encoding?: BufferEncoding) => boolean;
-        getConfig: <T_1>() => T_1;
-    };
-};
-export default _default;
